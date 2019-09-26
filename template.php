7ddac9420c6837205cf5d1af1eb248feecedacfd
Priyanto <priyayiman@gmail.com>
2019-09-26 13:24:10 +0700

    initial

---
+++ template.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @file
+ * The primary PHP file for this theme.
+ */
+
+function pacific_tuna_preprocess_html(&$variables) {
+  drupal_add_css('https://fonts.googleapis.com/css?family=PT+Serif');
+}
